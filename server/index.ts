86f27f7dc1f36e06b54485b1fa26c97eea35ad14
akamuinsaner <15108387504@163.com>
2019-08-28 23:15:28 +0800

    cache control

--- server/index.ts
+++ server/index.ts
@@ -9,11 +9,21 @@ class Server {
         this.port = 3456;
         this.root = path.resolve(process.cwd());
         this.indexPage = 'index.html';
+        this.enableCacheControl = true;
+        this.enableExpires = true;
+        this.enableETag = true;
+        this.enableLastModified = true;
+        this.maxAge = 100;
     }
 
     port: number;
     root: string;
     indexPage: string;
+    enableCacheControl: boolean;
+    enableExpires: boolean;
+    enableETag: boolean;
+    enableLastModified: boolean;
+    maxAge: number;
 
     respondNotFound(req, res) {
         res.writeHead(404, {
@@ -22,16 +32,48 @@ class Server {
         res.end(`<h1>Not Found</h1><p>The requested URL ${req.url} was not found on this server.</p>`);
     }
 
-    respondFile(pathName, req, res) {
+    respondFile(pathName, res) {
         const readStream = fs.createReadStream(pathName);
         res.setHeader('Content-Type', mime.lookup(pathName));
         readStream.pipe(res);
     }
 
+    generateETag(stat) {
+        const mtime = stat.mtime.getTime().toString(16);
+        const size = stat.size.toString(16);
+        return `W/"${size}-${mtime}"`;
+    }
+
+    setFreshHeaders(stat, res) {
+        const lastModified = stat.mtime.toUTCString();
+        if (this.enableExpires) {
+            const expireTime = (new Date(Date.now() + this.maxAge * 1000)).toUTCString();
+            res.setHeader('Expires', expireTime);
+        }
+        if (this.enableCacheControl) {
+            res.setHeader('Cache-Control', `public, max-age=${this.maxAge}`);
+        }
+        if (this.enableLastModified) {
+            res.setHeader('Last-Modified', lastModified);
+        }
+        if (this.enableETag) {
+            res.setHeader('ETag', this.generateETag(stat));
+        }
+    }
+
+    isFresh(reqHeaders, resHeaders) {
+        const  noneMatch = reqHeaders['if-none-match'];
+        const  lastModified = reqHeaders['if-modified-since'];
+        if (!(noneMatch || lastModified)) return false;
+        if(noneMatch && (noneMatch !== resHeaders['etag'])) return false;
+        if(lastModified && lastModified !== resHeaders['last-modified']) return false;
+        return true;
+    }
+
     respondDirectory(pathName, req, res) {
         const indexPagePath = path.join(pathName, this.indexPage);
         if (fs.existsSync(indexPagePath)) {
-            this.respondFile(indexPagePath, req, res);
+            this.respondFile(indexPagePath, res);
         } else {
             fs.readdir(pathName, (err, files) => {
                 if (err) {
@@ -69,9 +111,13 @@ class Server {
         return path.endsWith('/');
     }
 
+    responseNotModified(res) {
+        res.writeHead(304);
+        res.end('');
+    }
+
     routeHandler(pathName, req, res) {
         fs.stat(pathName, (err, stat) => {
-            console.log(pathName)
             if (!err) {
                 const requestedPath = url.parse(req.url).pathname;
                 if (this.hasTrailingSlash(requestedPath) && stat.isDirectory()) {
@@ -79,7 +125,12 @@ class Server {
                 } else if (stat.isDirectory()) {
                     this.respondRedirect(req, res);
                 } else {
-                    this.respondFile(pathName, req, res);
+                    this.setFreshHeaders(stat, res);
+                    if (this.isFresh(req.headers, res._headers)) {
+                        this.responseNotModified(res);
+                    } else {
+                        this.respondFile(pathName, res);
+                    }
                 }
             } else {
                 this.respondNotFound(req, res);
